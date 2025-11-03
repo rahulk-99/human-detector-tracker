7dd8d311d10182cb5da6e192877a906d0973fd4b
rahul-99 <rahuliitdh789@gmail.com>
2025-11-03 18:35:32 -0500

    Fixed build

--- test/perception_test.cpp
+++ test/perception_test.cpp
@@ -655,8 +655,4 @@ TEST(GeometryUtilsTest, DepthEstimationEdgeCases) {
 }
 
 // Main function for GoogleTest
-int main(int argc, char** argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
 
